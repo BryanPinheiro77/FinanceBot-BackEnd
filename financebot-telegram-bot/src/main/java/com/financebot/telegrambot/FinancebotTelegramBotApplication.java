52a8de060c3897c35ebec61a4bb618eaf429a6b9
Bryan Pinheiro <bryan.pinheiro77@hotmail.com>
2026-06-02 22:23:34 -0300

    feat: add telegram conversation properties

--- financebot-telegram-bot/src/main/java/com/financebot/telegrambot/FinancebotTelegramBotApplication.java
+++ financebot-telegram-bot/src/main/java/com/financebot/telegrambot/FinancebotTelegramBotApplication.java
@@ -1,12 +1,16 @@
 package com.financebot.telegrambot;
 
 import com.financebot.telegrambot.config.TelegramBotProperties;
+import com.financebot.telegrambot.conversation.config.TelegramConversationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(TelegramBotProperties.class)
+@EnableConfigurationProperties({
+        TelegramBotProperties.class,
+        TelegramConversationProperties.class
+})
 public class FinancebotTelegramBotApplication {
 
     public static void main(String[] args) {
